469e8a49ddb7349cdf61b5c10887258e7a841a5a
esousacosta <victoremanoel23@gmail.com>
2023-11-17 20:48:12 +0100

    Add two-way data binding to form fields

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
-  styleUrls: ['./user-settings-form.component.css']
+  styleUrls: ['./user-settings-form.component.css'],
 })
 export class UserSettingsFormComponent {
+  originalSettings: UserSettings = {
+    name: 'Milton',
+    emailOffers: false,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Yearly',
+    notes: 'here are some notes...',
+  };
 
+  userSettings: UserSettings = { ...this.originalSettings };
 }
